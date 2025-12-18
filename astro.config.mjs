d2ffd0199515d017204b0d6c603e293ff9233ccf
Wing <114337707+wing-0902@users.noreply.github.com>
2025-12-18 09:45:59 +0000

    Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://acdfghijklmnopqruvw.xyz',
+  integrations: [alpinejs()],
 });
